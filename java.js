9e66328a63a0067ed3febc6e257a4dd3ac066d30
ProgrammerHoang <hoangnvph50168@gmail.com>
2025-01-03 22:54:28 +0700

    test commit

---
+++ java.js
@@ -0,0 +1,3 @@
+colum('viết tắt là .col(kế thừa thằng padding tức là dùng padding để chỉnh kích thước)'('cột'))
+row('viết tắt là .row(kế thừa thằng margin tương tự như column dùng để chỉnh kích thước)'('hàng'))
+//(lưu ý nếu 2 thg row và column chỉ dùng những khi trang web có 2 dữ liệu trở lên tức là: padding: 23px 40px 30px....)
